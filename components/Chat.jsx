92d50e68353926d55620a6fd6fd97f618e827867
Shelby <yeoxhshelby20.21@ichat.sp.edu.sg>
2022-11-12 23:08:19 +0800

    style: :lipstick: edited className for Chat.jsx
    idk why it changed but ok i changed it back

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -1,17 +1,17 @@
-import InputBar from "./InputBar";
-import ChatBubbles from "./ChatBubbles";
+import InputBar from './InputBar';
+import ChatBubbles from './ChatBubbles';
 
 const Chat = () => (
   <div>
-    <div className="absolute bottom-20 w-full">
+    <div className='absolute bottom-20 w-full'>
       <ChatBubbles />
     </div>
-    <div className="absolute bottom-2 left-1/4 w-1/2">
-      <div className="flex justify-center">
+    <div className='fixed bottom-2 w-3/4'>
+      <div className='flex justify-center'>
         <InputBar />
       </div>
     </div>
   </div>
-)
+);
 
 export default Chat;
